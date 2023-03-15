82bd607f68e069373b671c2d6dad73527b44856e
1212Hong <101040824+1212Hong@users.noreply.github.com>
2023-03-16 00:04:39 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 512 ms, Memory: 63964 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,31 +1,35 @@
-import java.util.*;
 import java.io.*;
-public class Main{
-    
-    public static void main(String [] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        final int NUMBER_OF_SITE = Integer.parseInt(st.nextToken());
-        final int NUMBER_OF_REQUEST = Integer.parseInt(st.nextToken());
-        
-        HashMap<String,String> site = new HashMap<String,String>();
-        for(int i=0;i<NUMBER_OF_SITE;i++) {
-            st = new StringTokenizer(br.readLine());
-            site.put(st.nextToken(), st.nextToken());
-        }
-        for(int i=0;i<NUMBER_OF_REQUEST;i++) {
-            bw.write(site.get(br.readLine())+"\n");
-        }
+import java.util.*;
 
-       
-        bw.flush();
-	br.close();
-	bw.close();
-        
-        
-    }
+public class Main {
+	static int N, M;
+	static int[] arr, numbers;
+	static StringBuilder sb = new StringBuilder();
 
- 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		Map<String, String> map = new HashMap<String, String>();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String A = st.nextToken();
+			String B = st.nextToken();
+			map.put(A, B);
+		}
+
+		for (int i = 0; i < M; i++) {
+			String s = br.readLine();
+			sb.append(map.get(s)).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
 }
